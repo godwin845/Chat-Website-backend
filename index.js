977d7ae696ae4142f8a248c57f8d38467cdb5cc3
Kutty <godwinraj845@gmail.com>
2026-01-20 14:37:02 +0530

    first commit

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import authRouter from  './rout/authUser.js'
 import messageRouter from './rout/messageRout.js'
 import userRouter from './rout/userRout.js'
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 
 import {app , server} from './Socket/socket.js'
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,11 +21,11 @@ app.use('/api/auth',authRouter)
 app.use('/api/message',messageRouter)
 app.use('/api/user',userRouter)
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
+// app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
-})
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+// })
 
 const HOST = process.env.HOST || '0.0.0.0'
 const PORT = process.env.PORT || 5000
