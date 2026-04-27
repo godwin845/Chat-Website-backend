a5424c37c430c3118b0b7f4b0cec9d1133dfc67d
Kutty <godwinraj845@gmail.com>
2026-04-27 16:48:26 +0530

    Updated

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        // origin: ["http://localhost:5173"],
+        origin: ["https://chat-web-si-te.netlify.app"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     })
